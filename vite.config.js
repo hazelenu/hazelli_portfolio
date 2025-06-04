72680753199badf7b1d7ada0af0979360858ddb9
hazelenu <elainelenu@gmail.com>
2025-06-04 04:50:14 -0700

    updated vite config and deploy script

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚠️ 将 username 和 repo-name 改成你自己的
 export default defineConfig({
-  base: '/portfolio-site/',  // very important!
-  plugins: [react()]
+  base: '/hazelli_portfolio/', // <--- ⚠️ 用你的 repo 名称（注意：带斜杠）
+  plugins: [react()],
 })
-
